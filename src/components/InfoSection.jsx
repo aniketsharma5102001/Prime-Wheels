5a2caf6a3d3375d5a6b92f6a8eddcd0f9e2475ac
aniketsharma5102001 <126319138+aniketsharma5102001@users.noreply.github.com>
2025-05-29 12:57:24 +0530

    fix: correct filr path casing

--- src/components/InfoSection.jsx
+++ src/components/InfoSection.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function infoSection() {
+function InfoSection() {
   return (
  <section>
   <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
@@ -32,4 +32,4 @@ function infoSection() {
   )
 }
 
-export default infoSection
+export default InfoSection;
